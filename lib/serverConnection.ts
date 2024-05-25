0b1b35ab18ac199c3ec57713f543c6f9ec7263e9
kingsley-amoke <klordbravo@gmail.com>
2024-05-25 01:04:30 +0100

    bug fixes

--- lib/serverConnection.ts
+++ lib/serverConnection.ts
@@ -1,30 +1,47 @@
 import { cookies } from "next/headers";
 
-import { createServerClient, CookieOptions } from "@supabase/ssr";
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
+const cookieStore = cookies()
+
 export const serverClient = () => {
-    return createServerClient(supabaseUrl, supabaseKey, {
-        cookies: {
-            get(name){
-                return cookies().get(name)?.value
-            },
-            set(name:string, value:string, options: CookieOptions){
-                try {
-                    cookies().set({name, value, ...options})
-                } catch (error) {
-                    console.log(error)
-                }
-            },
-            remove(name:string, options:CookieOptions){
-               try {
-                cookies().set({name, value: '', ...options})
-               } catch (error) {
-                console.log(error)
-               }
-            }
-        }
-    })
+
+  const supabase = createServerClient(
+    supabaseUrl,
+    supabaseKey,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        },
+      },
+    }
+  )
+
+  return supabase
+
+    // return createServerClient(supabaseUrl, supabaseKey, {
+    //     cookies: {
+    //         get(name){
+    //             return cookies().get(name)?.value
+    //         },
+    //         set(name:string, value:string, options: CookieOptions){
+    //             try {
+    //                 cookies().set({name, value, ...options})
+    //             } catch (error) {
+    //                 console.log(error)
+    //             }
+    //         },
+    //         remove(name:string, options:CookieOptions){
+    //            try {
+    //             cookies().set({name, value: '', ...options})
+    //            } catch (error) {
+    //             console.log(error)
+    //            }
+    //         }
+    //     }
+    // })
 }
