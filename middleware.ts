6cbbdfe190096ce1111daf62c5da9cb5138208c5
kingsley-amoke <klordbravo@gmail.com>
2024-06-02 09:51:38 +0100

    minor updates

--- middleware.ts
+++ middleware.ts
@@ -10,25 +10,25 @@ const {pathname} = req.nextUrl
 
 
 
-const {data:{user}} = await serverClient().auth.getUser()
+const data = await serverClient().auth.getUser()
 
-const loggedUser = await fetchUser(user?.email)
-
-
+const loggedUser = await fetchUser(data.data.user?.email)
 
 
-if(user && pathname == '/'){
+
+
+if(data.data.user && pathname == '/'){
     return NextResponse.redirect('https://alfasimdata.com.ng/dashboard?showDialog=y')
 }
-if(user && pathname == '/login'){
-    return NextResponse.redirect('https://alfasimdata.com.ng/dashboard?showDialog=y')
-}
-
-if(user && pathname == '/register'){
+if(data.data.user && pathname == '/login'){
     return NextResponse.redirect('https://alfasimdata.com.ng/dashboard?showDialog=y')
 }
 
-if(!user && loggedInPaths.includes(pathname)){
+if(data.data.user && pathname == '/register'){
+    return NextResponse.redirect('https://alfasimdata.com.ng/dashboard?showDialog=y')
+}
+
+if(!data.data.user && loggedInPaths.includes(pathname)){
     return NextResponse.redirect('https://alfasimdata.com.ng/login')
 }
 
