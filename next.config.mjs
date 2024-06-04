117a3e345f57e26a221f06541467bf292f06a587
kingsley-amoke <klordbravo@gmail.com>
2024-06-04 10:33:35 +0100

    updated image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    eslint: {
-        ignoreDuringBuilds: true,
-        images: {
-            domains: [
-              "i.ibb.co"
-            ],
-        }
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+    images: {
+      remotePatterns: [{ protocol: "https", hostname: "i.ibb.co" }],
     },
+  },
 };
 
 export default nextConfig;
