d963b70afce7783a36e0af97bf5ac3e53887d594
kingsley-amoke <klordbravo@gmail.com>
2024-06-27 10:19:14 +0100

    added hero image

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,8 +4,8 @@ import Button from "./Button";
 
 const Hero = () => {
   return (
-    <div className="md:h-screen w-full bg-teal-800 dark:bg-black md:flex justify-between items-center md:gap-64 p-10 text-white">
-      <div className="flex flex-col md:gap-32 gap-10">
+    <div className="md:h-screen w-full bg-teal-800 dark:bg-black md:flex justify-between items-center md:gap-5 p-10 text-white">
+      <div className=" md:w-1/2 flex flex-col md:gap-32 gap-10">
         <h2 className="uppercase font-bold md:text-5xl">
           Welcome to Alfasim Data
         </h2>
@@ -15,6 +15,15 @@ const Hero = () => {
           money.
         </p>
       </div>
+      <div className=" w-full flex justify-center items-center">
+        <Image
+          src="/hero.png"
+          alt="hero"
+          width={400}
+          height={400} 
+          className="md:w-[700px]"
+          />
+        </div>
       <div className="md:hidden h-full w-full flex flex-col gap-5 mt-5">
         <Button links="login">Login</Button>
 
