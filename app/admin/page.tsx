812f5b9d0fb595d830df279722487d3c275b885d
Kingsley Amoke <87276473+kingsley-amoke@users.noreply.github.com>
2024-06-29 19:04:14 +0100

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import AdminPage from '@/app/components/AdminPage'
 import { fetchUser, getLoggedUser } from '@/lib/data'
-
+import { Suspense } from 'react'
 
 const page = async() => {
   return (
     <div>
+<Suspense>
         
         <AdminPage />
+</Suspense>
     </div>
   )
 }
