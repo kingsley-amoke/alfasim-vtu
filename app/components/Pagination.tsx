c5efb2a02ff295ecbc8167509d7748cdf6faf111
kingsley-amoke <klordbravo@gmail.com>
2024-06-29 22:23:34 +0100

    minor fixes

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -11,30 +11,55 @@ import { useUsersStore } from "@/lib/store";
 import Button from "./Button";
 import { useRouter } from "next/navigation";
 
-const PaginationPage = ({currentPage, per_page}: {currentPage:string, per_page:string}) => {
+const PaginationPage = ({
+  currentPage,
+  per_page,
+}: {
+  currentPage: string;
+  per_page: string;
+}) => {
+  const router = useRouter();
 
-  const router = useRouter()
+  const { users } = useUsersStore();
 
-  const {users } = useUsersStore()
+  const start = (Number(currentPage) - 1) * Number(per_page);
+  const end = start + Number(per_page);
 
-  const start = (Number(currentPage) - 1) * (Number(per_page))
-  const end = start + (Number(per_page))
+  const hasNextPage = end < users.length;
+  const hasPreviousPage = start > 0;
 
-  const hasNextPage = end < users.length
-  const hasPreviousPage = start > 0
+  const handleNextPage = () => {
+    router.push(`/admin?page=${Number(currentPage) + 1}&per_page=${per_page}`);
+  };
+
+  const handlePreviousPage = () => {
+    router.push(`/admin?page=${Number(currentPage) - 1}&per_page=${per_page}`);
+  };
 
   return (
     <Pagination>
       <PaginationContent className="flex flex-row">
-          <Button className="flex bg-teal-800 text-white mr-5" onClick={() =>router.push(`/?page=${Number(currentPage) - 1}&per_page=${per_page}`)} disabled={!hasPreviousPage}>
+        <PaginationItem>
+          <button
+            className="flex bg-teal-800 text-white mr-5 py-2 px-3 justify-center items-center"
+            disabled={!hasPreviousPage}
+            onClick={() => handlePreviousPage()}
+          >
             Prev
-          </Button>
+          </button>
+        </PaginationItem>
         <PaginationItem>
           {currentPage}/{Math.ceil(10 / Number(per_page))}
         </PaginationItem>
-        <Button className="flex bg-teal-800 text-white ml-5" onClick={()=>{router.push(`/?page=${Number(currentPage) + 1}&per_page=${per_page}`)}} disabled={!hasNextPage}>
+        <PaginationItem>
+          <button
+            className="flex bg-teal-800 text-white ml-5 py-2 px-3 justify-center items-center"
+            disabled={!hasNextPage}
+            onClick={() => handleNextPage()}
+          >
             Next
-          </Button>
+          </button>
+        </PaginationItem>
       </PaginationContent>
     </Pagination>
   );
