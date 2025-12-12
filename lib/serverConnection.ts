ba115a70723dd75b4d4f67ad43a1f4ce9ec9210f
Kingsley Amoke <klordbravo@gmail.com>
2025-12-12 15:23:59 +0000

    fixed supabase server connection

--- lib/serverConnection.ts
+++ lib/serverConnection.ts
@@ -1,41 +1,34 @@
 import { cookies } from "next/headers";
 
-import { createServerClient, type CookieOptions } from "@supabase/ssr";
-import { NextResponse, NextRequest } from "next/server";
+import { createServerClient } from "@supabase/ssr";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const serverClient = () => {
-  const cookieStore = cookies();
+export const serverClient = async () => {
+  const cookieStore = await cookies();
 
-  
-    const supabase = createServerClient(
-      supabaseUrl,
-      supabaseKey,
-      {
-        cookies: {
-          get(name: string) {
-            return cookieStore.get(name)?.value
-          },
-          
-        },
-      }
-    )
+  const supabase = createServerClient(supabaseUrl, supabaseKey, {
+    cookies: {
+      get(name: string) {
+        return cookieStore.get(name)?.value;
+      },
+    },
+  });
 
-    return supabase
+  return supabase;
 
-//   return createServerClient(supabaseUrl, supabaseKey, {
-//     cookies: {
-//       get(name) {
-//         return cookieStore.get(name)?.value;
-//       },
-//       set(name: string, value: string, options: CookieOptions) {
-//         cookieStore.set({ name, value, ...options });
-//       },
-//       remove(name: string, options: CookieOptions) {
-//         cookieStore.set({ name, value: "", ...options });
-//       },
-//     },
-//   });
+  //   return createServerClient(supabaseUrl, supabaseKey, {
+  //     cookies: {
+  //       get(name) {
+  //         return cookieStore.get(name)?.value;
+  //       },
+  //       set(name: string, value: string, options: CookieOptions) {
+  //         cookieStore.set({ name, value, ...options });
+  //       },
+  //       remove(name: string, options: CookieOptions) {
+  //         cookieStore.set({ name, value: "", ...options });
+  //       },
+  //     },
+  //   });
 };
